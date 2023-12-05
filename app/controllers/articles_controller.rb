5222b53349d66bab4054a8ba59e93d51002e9fed
Saul Lugo <saullugolugo@gmail.com>
2023-12-05 21:50:37 +0100

    adding validation messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,25 @@ class ArticlesController < ApplicationController
   end
 
   def index
+    #debugger
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+    #debugger
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = "Article created!"
+      redirect_to article_path(@article)
+    else
+      #render 'new'
+      render :new, status: :unprocessable_entity
+      #debugger
+    end
+  end
 end
